fc50332b164f88567bb92626326d871827ec7410
cpenama <145102216+cpenama@users.noreply.github.com>
2023-09-15 13:29:01 +0200

    Add files via upload

---
+++ ex00/backgroound.js
@@ -0,0 +1,3 @@
+function changecolor_Run(){
+    document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
